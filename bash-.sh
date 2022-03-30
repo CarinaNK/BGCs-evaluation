9f5c78fd2cb0fa8673027729388db9f78a0446d3
SigneMCF <100410002+SigneMCF@users.noreply.github.com>
2022-03-30 22:38:02 +0200

    Add files via upload

---
+++ bash-.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+#depends on
+# antismash
+# ucsc fasplit
+
+# clean up.
+
+		rm -r ./subfas
+		rm -r ./anti_out
+		rm -r ./resultsgbs
+		rm summary.csv
+		
+# make directories
+mkdir ./subfas
+mkdir ./anti_out
+mkdir ./resultsgbs
+mkdir ./vout
+> summary.csv
+
+#split the fasta
+
+faSplit byname Gubaphage_genomes.fa ./subfas/
+
+conda activate antismash
+# run antismash
+find subfas/ -name "*.fa" | parallel -j 20 'antismash --minimal -c 1 --genefinding-tool prodigal  --output-dir anti_out/{/.} {}  '
+
+# cleanup
+python3 pythonattempt.py
+
+# takes all the relevant sequence files and moves them
+python3 genbankoutput.py
+
+# creation of summary file
+python3 summaryfile.py
+
+# concatenate fasta
+cd sortedfasta
+cat *.fa > combined.fa
+cd ~
+
+cp /sortedfasta/combined.fa ./
+conda deactivate
+
+# vcontact
+conda activate vContact2
+prodigal -i combined.fa -o gut_viral_genomes.genes -a gut_viral_genomes.faa -p meta
+vcontact2_gene2genome -p gut_viral_genomes.faa -o gut_viral_genomes_g2g.csv -s 'Prodigal-FAA'
+
+vcontact2 --raw-proteins gut_viral_genomes.faa --rel-mode 'Diamond' --proteins-fp gut_viral_genomes_g2g.csv --db 'None' --pcs-mode MCL --vcs-mode ClusterONE  --output-dir ./vout -v
+conda deactivate
+
+		

035fd4a33a2a2fcf90382544369be790ed7baba6
SigneMCF <100410002+SigneMCF@users.noreply.github.com>
2022-04-12 12:44:49 +0200

    Update genbankoutput.py

--- genbankoutput.py
+++ genbankoutput.py
@@ -36,8 +36,6 @@ for f in dirs:
 
         print ('Target path doesnt exist!')
         
-for files in  os.listdir('{}/resultgbs'.format(dirpath)):
-    name=os.path.splitext('{}'.format(files))[0]
-    shutil.copy2("{d}/subfas/{nam}.fa".format(d=dirpath,nam=name), "{}/sortedfasta/".format(dirpath))
+
 
 

366c7f1e4a0dd75eca5485a0356835c33e49d5db
SigneMCF <100410002+SigneMCF@users.noreply.github.com>
2022-04-12 11:50:44 +0200

    Update genbankoutput.py

--- genbankoutput.py
+++ genbankoutput.py
@@ -16,9 +16,9 @@ dirs=next(os.walk('{}/anti_out'.format(dirpath)))[1]
 
 for f in dirs:
   currentpath="{dpath}/anti_out/{fil}".format(dpath=dirpath,fil=f)
-  shutil.copy2("{di}/anti_out/{paths}/{fil}.gbk".format(paths=f,fil=f,di=dirpath), "{}/resultgbs/".format(dirpath))
   print("{di}/anti_out/{paths}/{fil}.gbk".format(paths=f,fil=f,di=dirpath))
   print( "to {}/resultgbs/".format(dirpath))
+  shutil.copy2("{di}/anti_out/{paths}/{fil}.gbk".format(paths=f,fil=f,di=dirpath), "{}/resultgbs/".format(dirpath))
   os.remove("{di}/anti_out/{paths}/{fil}.gbk".format(paths=f,fil=f,di=dirpath))
   os.remove("{di}/anti_out/{paths}/{fil}.json".format(paths=f,fil=f,di=dirpath))
 

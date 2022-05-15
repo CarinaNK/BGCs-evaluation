33e3ca5f1a03864344683dffae117ca82ddc22fc
SigneMCF <100410002+SigneMCF@users.noreply.github.com>
2022-05-15 13:14:56 +0200

    Create BigscapeBash.sh

---
+++ BigscapeBash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ./sortedfasta
+makeblastdb -in /mnt/raid2/s194695/1Apr2022_genomes.fa -dbtype nucl -input_type fasta -out phagDB
+
+rm -r ./blastres
+mkdir ./blastres
+find ./ -name "*.fa" | parallel -j 12 -a - blastn -task 'dc-megablast' -db phagDB -query {} -max_target_seqs 1 -outfmt 6 -out ./blastres/{.}.txt		
+
+ 
+
+
+ls ./blastres | wc -l
+
+python ./BiG-SCAPE/bigscape.py -i ./anti_out -o ./scapeout  --pfam_dir ./ -c 10 --include_singletons 

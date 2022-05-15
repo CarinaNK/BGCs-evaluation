e683d597118a0c05425b4722e5c6385f6cd3883c
SigneMCF <100410002+SigneMCF@users.noreply.github.com>
2022-05-15 13:06:37 +0200

    Update and rename bash-.sh to AntiSMASHbash.sh

--- AntiSMASHbash.sh
+++ AntiSMASHbash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #depends on
+# run in Antismash environment
 # antismash
 # ucsc fasplit
 
@@ -23,7 +24,6 @@ mkdir ./vout
 > summary.csv
 
 #split the fasta
-conda activate antismash
 faSplit byname GPD_sequences.fa ./subfas/
 
 
@@ -45,14 +45,6 @@ cat *.fa > combined.fa
 cd ~
 
 cp /sortedfasta/combined.fa ./
-conda deactivate
-
-# vcontact
-conda activate vContact2
-prodigal -i combined.fa -o gut_viral_genomes.genes -a gut_viral_genomes.faa -p meta
-vcontact2_gene2genome -p gut_viral_genomes.faa -o gut_viral_genomes_g2g.csv -s 'Prodigal-FAA'
-
-vcontact2 --raw-proteins gut_viral_genomes.faa --rel-mode 'Diamond' --proteins-fp gut_viral_genomes_g2g.csv --db 'None' --pcs-mode MCL --vcs-mode ClusterONE  --output-dir ./vout -v
 conda deactivate
 
 		
